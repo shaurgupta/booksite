55af4b5afd4d6b861fe244f8dd5584ae4b21d09c
Shaurya Gupta <shauryamonuc3@gmail.com>
2020-04-04 16:26:40 +0530

    Login page cosntruction in preogress.

--- app.js
+++ app.js
@@ -9,12 +9,29 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/bookDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  googleID: String,
+});
 
-
-app.get("/",function(req,res){
+app.get("/login",function(req,res){
   res.render("login");
+});
+
+app.get("/signup",function(req,res){
+  res.render("signup");
+});
+
+
+app.post("/login", function(req,res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  user.save();
 });
 
 app.listen(3000, function(){
